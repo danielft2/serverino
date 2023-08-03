cb6c916f5b4ac44a935b9bc8898920335212946e
danielft2 <danielalmeida808z@gmail.com>
2023-08-02 22:30:04 -0300

    refactor: alterando mensagem de erro génerica

--- src/services/http/errors/index.ts
+++ src/services/http/errors/index.ts
@@ -1,6 +1,6 @@
 export const ERRORS_MESSAGES = {
    CREDENCIALS_INVALID: 'Usuário e/ou senha incorretos!',
    PHONE_ALREDY_EXISTS: 'Esse telefone já foi cadastrado!',
-   GENERIC_ERROR: 'Serviço indisponível, tente novamente mais tarde!',
+   GENERIC_ERROR: 'Algo deu errado, tente novamente!',
    ALREDY_EXISTS: '403'
 };
