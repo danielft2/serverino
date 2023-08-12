57000da1e14fa82a876110866379031c00f5a366
danielft2 <danielalmeida808z@gmail.com>
2023-08-12 15:43:00 -0300

    feat: implementando redicionamento dos botoões de contato

--- src/domain/types/index.ts
+++ src/domain/types/index.ts
@@ -2,10 +2,12 @@ import { AvatarOptions } from './avatar-options';
 import { InteractionTypes } from './interaction-types';
 import { InteractionActionProps } from './interaction-action-props';
 import { InteractionParams } from './interaction-params';
+import { TalkProfessionalType } from './talk-professional-types';
 
 export {
    AvatarOptions,
    InteractionTypes,
    InteractionActionProps,
-   InteractionParams
+   InteractionParams,
+   TalkProfessionalType
 };
--- src/templates/ProfessionalDetails/ProfessionalTalk.tsx
+++ src/templates/ProfessionalDetails/ProfessionalTalk.tsx
@@ -1,54 +1,93 @@
+import { useState } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { Phone, MessagesSquare } from 'lucide-react-native';
 import * as Linking from 'expo-linking';
 
+import { Loading } from '@components/Loading';
 import { Option } from '@components/Option';
-import { useFontsize } from '@hooks/shared';
+import { TalkProfessionalType } from '@domain/types';
+import { ERRORS_MESSAGES } from '@services/http/errors';
+import { useFontsize, useToast } from '@hooks/shared';
 
 interface ProfessionalTalkProps {
    numberPhone: string;
 }
 
 export function ProfessionalTalk({ numberPhone }: ProfessionalTalkProps) {
+   const [isLoading, setIsLoading] = useState(false);
    const { getFontsize } = useFontsize();
+   const { showBasicMessage } = useToast();
 
-   // const talkWithProfessional = async () =>
-   //    await Linking.openURL(`whatsapp://send?text=Olá&phone=+55${numberPhone}`);
+   async function talkWithProfessional(
+      talkProfessionalType: TalkProfessionalType
+   ) {
+      setIsLoading(true);
+      try {
+         if (talkProfessionalType === TalkProfessionalType.WHATSAPP) {
+            await Linking.openURL(
+               `whatsapp://send?text=Olá&phone=+55${numberPhone}`
+            );
+         } else if (talkProfessionalType === TalkProfessionalType.PHONE) {
+            await Linking.openURL(`tel:+55${numberPhone}`);
+         }
+      } catch (error) {
+         showBasicMessage(ERRORS_MESSAGES.GENERIC_ERROR);
+      } finally {
+         setIsLoading(false);
+      }
+   }
 
    return (
       <View className="w-full flex-1 flex-row items-end space-x-2 base:mb-3 base:px-3 sm:px-4 md:mb-4">
          <View className="flex-1">
-            <Option.Root isBackground>
-               <Option.Container>
-                  <Option.Icon>
-                     <MessagesSquare
-                        size={getFontsize(20)}
-                        className="text-green-400"
-                     />
-                  </Option.Icon>
-                  <Text
-                     className="font-heading_md text-white"
-                     style={{ fontSize: getFontsize(11) }}
-                  >
-                     Whatsapp
-                  </Text>
-               </Option.Container>
-            </Option.Root>
+            <TouchableOpacity
+               onPress={() =>
+                  talkWithProfessional(TalkProfessionalType.WHATSAPP)
+               }
+            >
+               <Option.Root isBackground>
+                  <Option.Container>
+                     <Option.Icon>
+                        {isLoading ? (
+                           <Loading.Default />
+                        ) : (
+                           <MessagesSquare
+                              size={getFontsize(20)}
+                              className="text-green-400"
+                           />
+                        )}
+                     </Option.Icon>
+                     <Text
+                        className="font-heading_md text-white"
+                        style={{ fontSize: getFontsize(11) }}
+                     >
+                        Whatsapp
+                     </Text>
+                  </Option.Container>
+               </Option.Root>
+            </TouchableOpacity>
          </View>
          <View className="flex-1">
-            <Option.Root isBackground>
-               <Option.Container>
-                  <Option.Icon>
-                     <Phone size={getFontsize(20)} className="text-gray-100" />
-                  </Option.Icon>
-                  <Text
-                     className="font-heading_md text-white"
-                     style={{ fontSize: getFontsize(11) }}
-                  >
-                     Telefone
-                  </Text>
-               </Option.Container>
-            </Option.Root>
+            <TouchableOpacity
+               onPress={() => talkWithProfessional(TalkProfessionalType.PHONE)}
+            >
+               <Option.Root isBackground>
+                  <Option.Container>
+                     <Option.Icon>
+                        <Phone
+                           size={getFontsize(20)}
+                           className="text-gray-100"
+                        />
+                     </Option.Icon>
+                     <Text
+                        className="font-heading_md text-white"
+                        style={{ fontSize: getFontsize(11) }}
+                     >
+                        Telefone
+                     </Text>
+                  </Option.Container>
+               </Option.Root>
+            </TouchableOpacity>
          </View>
       </View>
    );
